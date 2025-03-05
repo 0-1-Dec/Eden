c1afca15d4c900effcfad577e86ba2fc285eaabf
Heecheol01 <khc3967@naver.com>
2025-03-05 19:27:01 +0900

    c++파일 인코딩 변환
    utf-8로 변환

--- Source/Eden/Game/EGameMode.cpp
+++ Source/Eden/Game/EGameMode.cpp
@@ -13,12 +13,15 @@ AEGameMode::AEGameMode()
 		//DefaultPawnClass = ThirdPersonClassRef.Class;
 	//}
 	//DefaultPawnClass
+
+	// 폰 클래스 지정
 	static ConstructorHelpers::FClassFinder<APawn> DefaultPawnClassRef(TEXT("/Script/Eden.ECharacterPlayer"));
 	if (DefaultPawnClassRef.Class)
 	{
 		DefaultPawnClass = DefaultPawnClassRef.Class;
 	}
 
+	// 플레이어 컨트롤러 지정(플레이어 컨트롤러 블루프린트)
 	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerClassRef(TEXT("/Script/Eden.EPlayerController"));
 	if (PlayerControllerClassRef.Class)
 	{
--- Source/Eden/Player/EPlayerController.cpp
+++ Source/Eden/Player/EPlayerController.cpp
@@ -7,6 +7,7 @@ void AEPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// 플레이 모드 진입시 마우스 게임창 내부로 넣는 코드
 	FInputModeGameOnly GameOnlyInputMode;
 	SetInputMode(GameOnlyInputMode);
 }
